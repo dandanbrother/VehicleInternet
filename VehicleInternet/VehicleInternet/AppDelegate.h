6cb1978356cdc25212c5b6517c26c5c109f1b3d8
Joker <tomorooo@163.com>
2016-03-13 11:02:37 +0800

    新建项目

---
+++ VehicleInternet/VehicleInternet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VehicleInternet
+//
+//  Created by joker on 16/3/13.
+//  Copyright © 2016年 TomorJM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
